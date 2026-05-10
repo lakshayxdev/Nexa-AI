fbcdce1398a8001aca6fe4e438047e63072ce1dc
Lakshay <161314423+lakshayxdev@users.noreply.github.com>
2026-05-10 17:02:57 +0530

    Update authController.js

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -3,13 +3,13 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 
-// ================= REGISTER =================
+
 
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    // check if user already exists
+ 
     const userExists = await User.findOne({ email });
 
     if (userExists) {
@@ -18,10 +18,9 @@ const registerUser = async (req, res) => {
       });
     }
 
-    // hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // create user
+  
     const user = await User.create({
       name,
       email,
@@ -48,13 +47,12 @@ const registerUser = async (req, res) => {
 };
 
 
-// ================= LOGIN =================
 
 const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // check user
+   
     const user = await User.findOne({ email });
 
     if (!user) {
@@ -63,7 +61,7 @@ const loginUser = async (req, res) => {
       });
     }
 
-    // compare passwords
+   
     const isMatch = await bcrypt.compare(
       password,
       user.password
